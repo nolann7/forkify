5804f273670171ab4309d65c4b006c52624a3b33
nolann7 <nolann709@gmail.com>
2022-09-10 19:50:37 +0300

    delete fraction

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -128,7 +128,8 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? ing.quantity : ''
+        // ing.quantity ? new Fraction(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
