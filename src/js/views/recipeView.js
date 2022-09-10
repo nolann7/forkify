07d69531f9a9284583e97119ec246337e3c35e25
nolann7 <nolann709@gmail.com>
2022-09-10 19:46:18 +0300

    fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+import { Fraction } from '.fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -76,14 +76,16 @@ class RecipeView extends View {
             </div>
           </div>
 
-          <div class="recipe__user-generated ${this._data.key ? '': 'hidden'}">
+          <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
             <svg>
               <use href="${icons}#icon-user"></use>
             </svg>
           </div>
           <button class="btn--round btn--bookmark">
             <svg class="">
-              <use href="${icons}#icon-bookmark${this._data.bookmarked ? '-fill' : '' }"></use>
+              <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
             </svg>
           </button>
         </div>
